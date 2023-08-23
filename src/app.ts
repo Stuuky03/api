dae713054e32ce2409fa5fbb8704f1e98aeda178
albberrrt <albberrrt.yayay@gmail.com>
2023-08-23 11:08:32 -0300

    feat: created user/create route and configured CORS

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,12 @@
 import Fastify from "fastify";
+import cors from "@fastify/cors"
 
 const app = Fastify({
   logger: true,
 })
+
+app.register(cors, {
+  origin: true,
+});
 
 export { app };
--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,13 @@
 import { app } from "./app";
 
-app.listen({ port: 7777, host: "0.0.0.0" }).then(() => console.log('Hello Server!'));
+app.get("/", () => {
+  return "Hello World";
+})
+app.post("/user/create", (request) => {
+  const reply = JSON.stringify(request.body);
+
+  console.log(reply);
+  return reply;
+})
+
+app.listen({ port: 7777, host: "localhost" }).then(() => console.log('Hello Server!'));
