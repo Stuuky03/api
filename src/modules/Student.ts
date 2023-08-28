a08d0dc69e207f1410eea2dda36c5f310e9092ba
Pedro Schmitz <88664287+schmitz014@users.noreply.github.com>
2023-08-27 21:33:57 -0300

    feat: add student model

--- src/modules/Student.ts
+++ src/modules/Student.ts
@@ -2,21 +2,27 @@ import { createId } from "@paralleldrive/cuid2";
 
 export class Student {
   private readonly _id: string;
+  public firstName: string;
+  public lastName: string;
   public username: string;
   public email: string;
   public password: string;
-  public avatarUrl: string;
+  public profileImg: string;
+  public description: string;
 
   get id(): string {
     return this._id;
   }
 
   constructor(props: Omit<Student, 'id'>) {
-    const { username, email, password, avatarUrl } = props;
+    const { firstName, lastName, username, email, password, profileImg, description } = props;
+    this.firstName = firstName;
+    this.lastName = lastName;
     this.username = username;
     this.email = email;
     this.password = password;
-    this.avatarUrl = avatarUrl;
+    this.profileImg = profileImg;
+    this.description = description;
 
     this._id = createId();
   }
