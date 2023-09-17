c66da8b68dfed6651da960e5cd6d11ceb5c4b571
Pedro Schmitz <88664287+schmitz014@users.noreply.github.com>
2023-09-17 09:35:05 -0300

    feat: general queries added

--- src/infra/graphql/nexusTypes/mutation.ts
+++ src/infra/graphql/nexusTypes/mutation.ts
@@ -1,5 +1,73 @@
 import { mutationType } from "nexus";
 
-const Mutation = "Nothing here yet"
+const Mutation = mutationType({
+ definition (t) {
+   t.field('question', {
+     type: 'Question',
+     resolve: async (_parent, _args, context) => {
+       const newQuestion = await context.prisma.question.create({
+         data: {
+           title: _args.input.title,
+           content: _args.input.content,
+           createdAt: new Date(),
+           isDraft: false
+         }
+       })
+
+       try{
+         const delQuestion = await context.prisma.question.delete({
+           where: { id: _args.id }
+         })
+
+         return { success: true, message: 'Pergunta excluída.' }
+       } catch (error) {
+         console.log("Error deleting question", error);
+       }
+
+       const editQuestion = await context.prisma.question.update({
+         where: { id: _args.id },
+         data: {
+           title: _args.input.title,
+           content: _args.input.content,
+           createdAt: new Date(),
+           isDraft: false
+         }
+       }) 
+     }
+   })
+
+   t.field('stuuke', {
+    type: 'Stuuke',
+    resolve: async (_parent, _args, context) => {
+     const newStuuke = await context.prisma.stuuke.create({
+       data: {
+        title: _args.input.title,
+        content: _args.input.content,
+        createdAt: new Date(),
+       }
+     })
+
+     try{
+      const delStuuke = await context.prisma.stuuke.delete({
+        where: { id: _args.id }
+      })
+
+      return { success: true, message: 'Stuuke excluído.' }
+    } catch (error) {
+      console.log("Error deleting stuuke", error);
+    }
+
+    const editStuuke = await context.prisma.stuuke.update({
+     where: { id: _args.id },
+         data: {
+           title: _args.input.title,
+           content: _args.input.content,
+           createdAt: new Date(),
+         }
+    })
+    }
+   })
+ }
+})
 
 export default { Mutation }
--- src/infra/graphql/nexusTypes/query.ts
+++ src/infra/graphql/nexusTypes/query.ts
@@ -8,7 +8,23 @@ const Query = queryType({
         return context.prisma.student.findMany()
       }
     })
+
+    t.list.field('points',{
+      type: 'Student',
+      resolve: (_parent, _args, context) => {
+        return context.prisma.studentPoints.findMany()
+      }
+    })
+
+    t.list.field('badges', {
+      type: 'Student',
+      resolve: (_parent, _args, context) => {
+        return context.prisma.studentBadges.findMany()
+      }
+    })
   }
 })
+
+
 
 export default { Query }
